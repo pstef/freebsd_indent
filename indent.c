fb9a37d54ac092f770d1f3e7a8cf12b9739f8b1f
pstef <pstef@users.noreply.github.com>
2016-12-26 00:03:27 +0100

    [bugfix] Don't treat typedef declarations as object declarations.
    This avoids indenting them like variable names.

--- indent.c
+++ indent.c
@@ -914,6 +914,7 @@ check_type:
 	    }
 	    goto copy_id;	/* move the token into line */
 
+	case type_def:
 	case storage:
 	    prefix_blankline_requested = 0;
 	    goto copy_id;
@@ -932,7 +933,7 @@ check_type:
 	    }
 	    ps.in_or_st = true;	/* this might be a structure or initialization
 				 * declaration */
-	    ps.in_decl = ps.decl_on_line = true;
+	    ps.in_decl = ps.decl_on_line = ps.last_token != type_def;
 	    if ( /* !ps.in_or_st && */ ps.dec_nest <= 0)
 		ps.just_saw_decl = 2;
 	    prefix_blankline_requested = 0;
